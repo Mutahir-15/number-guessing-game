b656abac8b1ea7d09f765dd59dd38f94810c7b95
Mutahir-15 <164089040+Mutahir-15@users.noreply.github.com>
2024-03-23 01:11:54 +0500

    Project Guessing-game Done.

---
+++ main.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()*10+1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess a Number between 1 - 10:",
+    }
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations!!! you have Guessed the Right number.")
+}
+else {
+    console.log("Oops!! You Have Guessed the wrong Number.")
+}
